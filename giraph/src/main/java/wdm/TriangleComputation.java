62157998676bbd78d0da269b80c7e9ab27bcf668
Nick ten Veen <nveen0930@gmail.com>
2015-06-11 11:58:02 +0200

    first attempt of triangle counting

--- giraph/src/main/java/wdm/TriangleComputation.java
+++ giraph/src/main/java/wdm/TriangleComputation.java
@@ -1,16 +1,52 @@
 package wdm;
 
+import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.BasicComputation;
-import org.apache.giraph.graph.Computation;
 import org.apache.giraph.graph.Vertex;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
-public class TriangleComputation extends BasicComputation<DoubleWritable, DoubleWritable, FloatWritable, DoubleWritable> {
+
+public class TriangleComputation extends BasicComputation<Text, IntWritable, FloatWritable, Text> {
+
+    static final Logger log = Logger.getLogger(TriangleComputation.class);
+
     @Override
-    public void compute(Vertex<DoubleWritable, DoubleWritable, FloatWritable> vertex, Iterable<DoubleWritable> messages) throws IOException {
-        
+    public void compute(Vertex<Text, IntWritable, FloatWritable> vertex, Iterable<Text> messages) throws IOException {
+        switch((int) getSuperstep()){
+            case 0:
+                for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                    if(vertex.getId().compareTo(e.getTargetVertexId()) < 0){ //we have a smaller index
+                        sendMessage(e.getTargetVertexId(), vertex.getId());
+                    }
+                }
+                break;
+            case 1:
+                for(Text sourceId : messages){
+                    for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                        if(vertex.getId().compareTo(e.getTargetVertexId()) < 0){
+                            sendMessage(e.getTargetVertexId(), sourceId);
+                        }
+                    }
+                }
+                break;
+            case 2:
+                int trianglesFound = 0;
+                for(Text sourceId : messages){
+                    for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                        if(e.getTargetVertexId().compareTo(sourceId) == 0){
+                            trianglesFound++;
+                        }
+                    }
+                }
+                log.debug("triangles found: " + trianglesFound);
+                vertex.setValue(new IntWritable(trianglesFound));
+                break;
+        }
+        vertex.voteToHalt();
     }
 }
