3f9599e2769afb1807d9e0212ce022ef511c9ee7
Nick ten Veen <nveen0930@gmail.com>
2015-06-11 12:52:25 +0200

    changed id types

--- giraph/src/main/java/wdm/TriangleComputation.java
+++ giraph/src/main/java/wdm/TriangleComputation.java
@@ -3,31 +3,29 @@ package wdm;
 import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.Vertex;
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.log4j.Logger;
-
 import java.io.IOException;
 
 
-public class TriangleComputation extends BasicComputation<Text, IntWritable, FloatWritable, Text> {
+public class TriangleComputation extends BasicComputation<IntWritable, IntWritable, NullWritable, IntWritable> {
 
     static final Logger log = Logger.getLogger(TriangleComputation.class);
 
+
     @Override
-    public void compute(Vertex<Text, IntWritable, FloatWritable> vertex, Iterable<Text> messages) throws IOException {
+    public void compute(Vertex<IntWritable, IntWritable, NullWritable> vertex, Iterable<IntWritable> messages) throws IOException {
         switch((int) getSuperstep()){
             case 0:
-                for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                for(Edge<IntWritable, NullWritable> e : vertex.getEdges()){
                     if(vertex.getId().compareTo(e.getTargetVertexId()) < 0){ //we have a smaller index
                         sendMessage(e.getTargetVertexId(), vertex.getId());
                     }
                 }
                 break;
             case 1:
-                for(Text sourceId : messages){
-                    for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                for(IntWritable sourceId : messages){
+                    for(Edge<IntWritable, NullWritable> e : vertex.getEdges()){
                         if(vertex.getId().compareTo(e.getTargetVertexId()) < 0){
                             sendMessage(e.getTargetVertexId(), sourceId);
                         }
@@ -36,8 +34,8 @@ public class TriangleComputation extends BasicComputation<Text, IntWritable, Flo
                 break;
             case 2:
                 int trianglesFound = 0;
-                for(Text sourceId : messages){
-                    for(Edge<Text, FloatWritable> e : vertex.getEdges()){
+                for(IntWritable sourceId : messages){
+                    for(Edge<IntWritable, NullWritable> e : vertex.getEdges()){
                         if(e.getTargetVertexId().compareTo(sourceId) == 0){
                             trianglesFound++;
                         }
@@ -49,4 +47,5 @@ public class TriangleComputation extends BasicComputation<Text, IntWritable, Flo
         }
         vertex.voteToHalt();
     }
+
 }
